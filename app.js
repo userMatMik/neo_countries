afc60d5f2716212f571861a12e323fa8720bbaa0
userMatMik <106982809+userMatMik@users.noreply.github.com>
2022-09-19 23:02:40 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const API_URL = 'https://restcountries.com/v3.1/all';
 
 
-const renderCountriesList = (countriesList) => {
+const createCountryElement = (country) => {
+    const countryElement = document.createElement('li');
+    console.log(country)
+    return countryElement
+}
+
+const createCountriesList = (countriesList) => {
     const listElement = document.createElement('ul');
+    console.log(countriesList)
     countriesList.forEach((country) => {
-        listElement.appendChild(countryElement(country))
+        listElement.appendChild(createCountryElement(country))
     })
-} 
+    return listElement;
+}
+
+const renderCountriesList = (countriesList) => {
+    const mainElement = document.querySelector('#main');
+    mainElement.appendChild(createCountriesList(countriesList));
+}
 
 const fetchData = async () => {
     const response = await fetch(API_URL);
     const data = await response.json();
-    console.log(data)
+    // console.log(data)
     const countriesList = data.map((country) => {
         return {
             name: country.name.common,
