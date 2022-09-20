5ffa97d39a0d8975ee36305e606c9102bdd3cb96
userMatMik <106982809+userMatMik@users.noreply.github.com>
2022-09-20 20:16:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
 const API_URL = 'https://restcountries.com/v3.1/all';
 
 
+const createInfoElement = (str, value) => {
+    const infoDivElement =  document.createElement('div');
+    
+    const labelElement = document.createElement('strong');
+    labelElement.innerText = `${str}: `;
+
+    const valueElement = document.createElement('span');
+    valueElement.innerText = value;
+
+    infoDivElement.appendChild(labelElement);
+    infoDivElement.appendChild(valueElement);
+
+    return infoDivElement;
+}
+
+const createInfoContainer = (country) => {
+    const infoContainerElement = document.createElement('div');
+
+    const countryNameElement = document.createElement('h2');
+    countryNameElement.innerText = country.name
+
+    infoContainerElement.appendChild(countryNameElement);
+
+    infoContainerElement.appendChild(createInfoElement("Capital", country.capital));
+    infoContainerElement.appendChild(createInfoElement("Region", country.region));
+    infoContainerElement.appendChild(createInfoElement("Population", country.population));
+
+
+    return infoContainerElement;
+}
+
+const createFlagElement = (country) => {
+    const flagContainerElement = document.createElement('div');
+    const flagImgElement = document.createElement('img');
+
+    flagImgElement.src = country.flagUrl;
+    flagImgElement.alt = `${country.name} flag`;
+
+    flagContainerElement.appendChild(flagImgElement);
+
+    return flagContainerElement;
+}
+
 const createCountryElement = (country) => {
     const countryElement = document.createElement('li');
-    console.log(country)
+    
+    const linkElement = document.createElement('a');
+    linkElement.href = `?country=${country.countryID}`
+    
+    linkElement.appendChild(createFlagElement(country))
+
+    countryElement.appendChild(linkElement);
+    countryElement.appendChild(createInfoContainer(country));
+
     return countryElement
 }
 
 const createCountriesList = (countriesList) => {
     const listElement = document.createElement('ul');
-    console.log(countriesList)
+    // console.log(countriesList)
     countriesList.forEach((country) => {
         listElement.appendChild(createCountryElement(country))
     })
@@ -32,6 +83,7 @@ const fetchData = async () => {
             population: country.population,
             region: country.region,
             flagUrl: country.flags.png,
+            countryID: country.cioc,
         };
     } )
     
